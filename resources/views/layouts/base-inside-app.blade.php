b695d2a80d49c8b16dd0361c60b6890933457582
Orlando Padrón <31983869+OrlandoPadron@users.noreply.github.com>
2020-07-13 12:10:56 +0100

    Some refactoring for file uploading scripts

--- resources/views/layouts/base-inside-app.blade.php
+++ resources/views/layouts/base-inside-app.blade.php
@@ -30,6 +30,8 @@
     <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
     <link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
 
+    <!-- Firebase Scripts -->
+    @include('scripts.firebaseScripts')
 
     @yield('head')
 </head>
---
+++ resources/views/scripts/firebaseScripts.blade.php
@@ -0,0 +1,223 @@
+<script>
+    
+    /**
+     * Upload the file to Firebase. Depending on the method value, 
+     * different situations will be issued. 
+     * @params fileOwnerUserId -> user who uploads the file
+     * @params sharedWithUserId -> user target to share the file
+     * @params method -> determines the type of entity is calling the function 
+     *
+    */
+    function uploadFile(fileOwnerUserId, sharedWithUserId, method){
+        // Auth
+        firebase.auth().signInAnonymously().catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("Error " + errorCode + ' ' + errorMessage);
+        });
+        var file;
+        var storageRef;
+        var task;
+        var uploader;
+
+        switch (method){
+            case 'AthleteFileSection':
+                //Get file 
+                file = document.getElementById("file-upload").files[0];
+        
+                //Create storage ref
+                storageRef = firebase.storage().ref('users/'+fileOwnerUserId+'/files/'+ file.name);
+        
+        
+                //Upload file 
+                task = storageRef.put(file);
+        
+                //Update progress bar
+                uploader = document.getElementById("uploader");
+                break;
+        }
+
+
+        task.on('state_changed', 
+            function progress(snapshot){
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                uploader.value = progress;
+            },
+
+            function error(err){
+                console.log(err.message_);
+
+            },
+
+            function complete(){
+                task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                    console.log('File available at', downloadURL); 
+                    saveFileReferenceIntoDatabase(file, fileOwnerUserId, sharedWithUserId, downloadURL);
+                });
+            }
+        );
+
+
+    }
+
+    /**
+     * The method saves the relation between the file in firebase and 
+     * its database representation. 
+     * @params file -> file occurrence.
+     * @params fileOwnerUserId -> user who uploads the file
+     * @params sharedWithUserId -> user target to share the file
+     * @params downloadURL -> firebase's URL linked to the file
+     *
+    */
+    function saveFileReferenceIntoDatabase(file, fileOwnerUserId, sharedWithUserId, downloadURL){
+        var fileName = file.name.split('.').slice(0, -1).join('.');
+        var fileExtension = file.name.split('.').pop();
+        $.ajax({
+            url: "{{route("userFile.store")}}",
+            type: "POST",
+            data: {
+                file_name: fileName,
+                extension: fileExtension,
+                size: file.size,
+                url: downloadURL,
+                owned_by: fileOwnerUserId,
+                shared_with: sharedWithUserId,
+
+                _token: "{{csrf_token()}}",
+            },
+            success: function(){
+                alert("Su archivo '" + fileName +"' se ha subido correctamente."); 
+                location.reload();
+                
+            },
+            error: function(){
+                alert('Se ha producido un error.');
+                console.log('Error on ajax call "saveFileReferenceIntoDatabase" function');
+            }  
+        });
+    }
+
+    
+    /**
+     * Deletes user's file from firebase server. 
+     * @params fileOwnerUserId -> user who uploads the file
+     * @params fileName -> file's name
+     * @params fileId -> file's id
+     *
+    */
+    function deleteUserFile(fileOwnerUserId, fileName, fileId){
+        // Auth
+        firebase.auth().signInAnonymously().catch(function(error) {
+        // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("Error " + errorCode + ' ' + errorMessage);
+        });
+
+        //Create storage ref
+        var storageRef = firebase.storage().ref('users/'+fileOwnerUserId+'/files/'+ fileName);
+        
+
+        // Delete the file
+        storageRef.delete().then(function() {
+            // File deleted successfully
+            deleteFileReferenceFromDatabase(fileId, fileName);
+        }).catch(function(error) {
+            // Uh-oh, an error occurred!
+            console.log("No tan guay");
+
+        });
+    }
+
+    /**
+     * Deletes user's file from database. 
+     * @params fileId -> file's id
+     * @params fileName -> file's name
+     *
+    */
+
+    function deleteFileReferenceFromDatabase(fileId, fileName){
+        $.ajax({
+            url: "{{route("userFile.destroy")}}",
+            type: "POST",
+            data: {
+                fileId: fileId,
+                _token: "{{csrf_token()}}",
+            },
+            success: function(){
+                alert("Su archivo '" + fileName +"' se ha eliminado."); 
+                location.reload();
+                
+            },
+            error: function(){
+                alert('Se ha producido un error.');
+                console.log('Error on ajax call "deleteFileReferenceFromDatabase" function');
+            }  
+        });
+
+    }
+
+    /**
+     * Shares an already uploaded file to a specific user's profile. 
+     * @params userId -> user who is receiving the shared file.
+    */
+
+    function shareFile(userId){
+
+        var fileId = $("#filesNotShared").children("option:selected").val();
+        var fileName = $("#filesNotShared").children("option:selected").text();
+
+        $.ajax({
+            url: "{{route("userFile.update")}}",
+            type: "POST",
+            data: {
+                fileId: fileId,
+                userId: userId,
+                method: 'shareFile',
+
+                _token: "{{csrf_token()}}",
+            },
+            success: function(){
+                alert("Ha compartido su archivo '" + fileName +"' con {{$user->name}}."); 
+                location.reload();
+                
+            },
+            error: function(){
+                alert('Se ha producido un error.');
+                console.log('Error on ajax call "shareFile" function');
+            }  
+        });
+    }
+
+    
+    /**
+     * Stops sharing a given file with a given user. 
+     * @params fileId -> file's id
+     * @params userId -> user who has the shared file.
+     * @params fileName -> file's name
+    */
+    function stopSharingFile(fileId, userId, fileName){
+        $.ajax({
+            url: "{{route("userFile.update")}}",
+            type: "POST",
+            data: {
+                fileId: fileId,
+                userId: userId,
+                method: 'stopSharing',
+
+                _token: "{{csrf_token()}}",
+            },
+            success: function(){
+                alert("Su archivo '" + fileName +"' se ha dejado de compartir con {{$user->name}}."); 
+                location.reload();
+                
+            },
+            error: function(){
+                alert('Se ha producido un error.');
+                console.log('Error on ajax call "stopSharingFile" function');
+            }  
+        });
+    }
+</script>
